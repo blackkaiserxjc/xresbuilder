60e0e0cb76181f66930eda7fd9ed8eb36a5da9d1
alien <hadesxjc@gmail.com>
2021-06-01 18:02:19 +0800

    format

--- src/kr/core/type.h
+++ src/kr/core/type.h
@@ -3,18 +3,20 @@
 namespace kr {
 namespace core {
 /** 数据类型 */
-enum BaseType {
-  BASE_TYPE_NONE = 0,
-  BASE_TYPE_BOOL = 1,
-  BASE_TYPE_INT = 2,
-  BASE_TYPE_FLOAT = 3,
-  BASE_TYPE_STRING = 4,
-  BASE_TYPE_ARRAY = 5,
-  BASE_TYPE_OBJECT = 6
+enum BaseType 
+{
+    BASE_TYPE_NONE = 0,
+    BASE_TYPE_BOOL = 1,
+    BASE_TYPE_INT = 2,
+    BASE_TYPE_FLOAT = 3,
+    BASE_TYPE_STRING = 4,
+    BASE_TYPE_ARRAY = 5,
+    BASE_TYPE_OBJECT = 6
 };
 
-inline bool IsPod(BaseType type) {
-  return type >= BASE_TYPE_BOOL && type <= BASE_TYPE_STRING;
+inline bool IsPod(BaseType type) 
+{
+    return type >= BASE_TYPE_BOOL && type <= BASE_TYPE_STRING;
 };
 inline bool IsBool(BaseType type) { return type == BASE_TYPE_BOOL; }
 inline bool IsInt(BaseType type) { return type == BASE_TYPE_INT; }
@@ -23,27 +25,29 @@ inline bool IsString(BaseType type) { return type == BASE_TYPE_STRING; }
 inline bool IsArray(BaseType type) { return type == BASE_TYPE_ARRAY; }
 inline bool IsObject(BaseType type) { return type == BASE_TYPE_OBJECT; };
 
-struct FieldDef;
 struct ObjectDef;
-struct Type {
-  explicit Type(BaseType base_type = BASE_TYPE_NONE,
-                std::shared_ptr<ObjectDef> obj_def = nullptr,
-                std::uint16_t fixed_length = 0)
-      : base_type(base_type), element(BASE_TYPE_NONE), obj_def(obj_def),
-        fixed_length(fixed_length) {}
+struct Type
+{
+    explicit Type(BaseType base_type = BASE_TYPE_NONE,
+                  std::shared_ptr<ObjectDef> obj_def = nullptr,
+                  std::uint16_t fixed_length = 0)
+        : base_type(base_type), element(BASE_TYPE_NONE), obj_def(obj_def),
+          fixed_length(fixed_length) {}
 
-  bool operator==(const Type &t) {
-    return base_type == t.base_type && element == t.element &&
-           obj_def.get() == t.obj_def.get();
-  }
+    bool operator==(const Type &t)
+    {
+      return base_type == t.base_type && element == t.element &&
+            obj_def.get() == t.obj_def.get();
+    }
 
-  BaseType base_type;
-  BaseType element;
-  std::shared_ptr<ObjectDef> obj_def;
-  uint16_t fixed_length;
+    BaseType base_type;
+    BaseType element;
+    std::shared_ptr<ObjectDef> obj_def;
+    uint16_t fixed_length;
 };
 
-struct Value {
+struct Value
+{
   Value() : constant("0"), offset(0) {}
   Type type;
   std::string constant;
@@ -51,51 +55,58 @@ struct Value {
 };
 
 /** 符号表 */
-template <class T> class SymbolTable {
+template <class T>
+class SymbolTable
+{
 public:
-  SymbolTable() = default;
-  ~SymbolTable() = default;
+    SymbolTable() = default;
+    ~SymbolTable() = default;
 
-  bool Add(const std::string &name, std::shared_ptr<T> element) {
-    vec.emplace_back(element);
-    if (auto iter = dict.find(name); iter != dict.end()) {
-      return true;
+    bool Add(const std::string &name, std::shared_ptr<T> element)
+    {
+        vec.emplace_back(element);
+        if (auto iter = dict.find(name); iter != dict.end())
+        {
+          return true;
+        }
+        dict[name] = element;
+        return false;
     }
-    dict[name] = element;
-    return false;
-  }
 
-  std::shared_ptr<T> Lookup(const std::string &name) const {
-    auto iter = dict.find(name);
-    return iter == dict.end() ? nullptr : iter->second;
-  }
+    std::shared_ptr<T> Lookup(const std::string &name) const
+    {
+        auto iter = dict.find(name);
+        return iter == dict.end() ? nullptr : iter->second;
+    }
 
-  std::size_t Size() const { return dict.size(); }
+    std::size_t Size() const { return dict.size(); }
 
 public:
-  std::unordered_map<std::string, std::shared_ptr<T>> dict;
-  std::vector<std::shared_ptr<T>> vec;
+    std::unordered_map<std::string, std::shared_ptr<T>> dict;
+    std::vector<std::shared_ptr<T>> vec;
 };
 
-/** field, object 基类*/
-struct Definition {
-  Definition() : name{}, index{-1} {}
+struct Definition
+{
+    Definition() : name{}, index{-1} {}
 
-  int index;
-  std::string name;
+    int index;
+    std::string name;
 };
 
-struct FieldDef : public Definition {
-  FieldDef() : key(false) {}
+struct FieldDef : public Definition
+{
+    FieldDef() : key(false) {}
 
-  Value value;
-  bool key;
-  std::uint32_t begin_index;
-  std::uint32_t end_index;
+    Value value;
+    bool key;
+    std::uint32_t begin_index;
+    std::uint32_t end_index;
 };
 
-struct ObjectDef : public Definition {
-  SymbolTable<FieldDef> fields;
+struct ObjectDef : public Definition
+{
+    SymbolTable<FieldDef> fields;
 };
 
 } // namespace core
