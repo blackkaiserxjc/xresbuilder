02ff58158f9420ffc81fc99c484f83c7b3afdfb1
OrAlien <1535435878@qq.com>
2021-06-02 23:13:54 +0800

    删除注释

--- src/kr/app/main.cpp
+++ src/kr/app/main.cpp
@@ -2,19 +2,6 @@
 
 #include <kr/core/compiler.h>
 
-/*
-void data_loader_test()
-{
-    using namespace kr::core;
-    DataTable dt("data table");
-    if (DataLoader::execute("/home/alien/doc/3.csv", dt))
-    {
-        Model model(dt);
-        generate_json(model, "/home/alien/doc/", "json_test");
-        generate_lua(model, "/home/alien/doc/", "lua_test");
-    }
-}*/
-
 int main(int argc, char **argv) {
   using namespace kr::core;
   const Compiler::Generator generators[] = {
